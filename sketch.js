f1af9c8f3fbe47e946900a662b21e888eb855021
2009ryan <87080122+2009ryan@users.noreply.github.com>
2021-07-08 21:25:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+
+ a = prompt('Enter the value for A: ');
+b=prompt("Enter the value for B:");
+function setup(){
+  var b2 = createButton("click here to Swap");
+}
+ // decide the game area 
+createCanvas(800,800)
+
+function draw(){
+background("black")
+}
+function Swap (){
+[a,b]={b,a}
+
